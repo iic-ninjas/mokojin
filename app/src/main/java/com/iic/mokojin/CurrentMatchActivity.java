fcd3c84542ec80c0048698a6d78eeee9f4bd303c
Gilad Goldberg <giladgo@gmail.com>
2015-03-03 11:22:17 +0200

    First xml

--- app/src/main/java/com/iic/mokojin/CurrentMatchActivity.java
+++ app/src/main/java/com/iic/mokojin/CurrentMatchActivity.java
@@ -1,13 +1,9 @@
 package com.iic.mokojin;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class CurrentMatchActivity extends ActionBarActivity {
@@ -18,7 +14,7 @@ public class CurrentMatchActivity extends ActionBarActivity {
         setContentView(R.layout.activity_current_match);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new CurrentMatchFragment())
                     .commit();
         }
     }
@@ -46,19 +42,4 @@ public class CurrentMatchActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_current_match, container, false);
-            return rootView;
-        }
-    }
 }
---
+++ app/src/main/java/com/iic/mokojin/CurrentMatchFragment.java
@@ -0,0 +1,23 @@
+package com.iic.mokojin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class CurrentMatchFragment extends Fragment {
+
+    public CurrentMatchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_current_match, container, false);
+        return rootView;
+    }
+}
