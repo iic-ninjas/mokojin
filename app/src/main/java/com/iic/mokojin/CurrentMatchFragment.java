985165354d34731648660cb8dd2da92f9021edc1
Gilad Goldberg <giladgo@gmail.com>
2015-03-04 16:13:31 +0200

    Moved character display to a separate view

--- app/src/main/java/com/iic/mokojin/CurrentMatchFragment.java
+++ app/src/main/java/com/iic/mokojin/CurrentMatchFragment.java
@@ -1,22 +1,19 @@
 package com.iic.mokojin;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.iic.mokojin.models.Match;
 import com.iic.mokojin.models.Models;
 import com.iic.mokojin.models.Player;
 import com.iic.mokojin.operation.EndMatchOperation;
+import com.iic.mokojin.views.CharacterViewer;
 import com.parse.ParseException;
 
 import bolts.Continuation;
@@ -39,12 +36,11 @@ public class CurrentMatchFragment extends Fragment {
     @InjectView(R.id.players_container) View mPlayersContainer;
 
     @InjectView(R.id.player_a_name)  TextView mPlayerANameTextView;
-    @InjectView(R.id.player_a_image_front) ImageView mPlayerAImageViewFront;
-    @InjectView(R.id.player_a_image_back) ImageView mPlayerAImageViewBack;
-
     @InjectView(R.id.player_b_name)  TextView mPlayerBNameTextView;
-    @InjectView(R.id.player_b_image_front) ImageView mPlayerBImageViewFront;
-    @InjectView(R.id.player_b_image_back) ImageView mPlayerBImageViewBack;
+
+    @InjectView(R.id.player_a_character) CharacterViewer mPlayerACharacter;
+    @InjectView(R.id.player_b_character) CharacterViewer mPlayerBCharacter;
+
 
     public CurrentMatchFragment() {
     }
@@ -82,47 +78,6 @@ public class CurrentMatchFragment extends Fragment {
         refreshCurrentMatch();
     }
 
-    private void displayCharacter(int characterId, ImageView imageView) {
-        int resourceId = getResources().getIdentifier("player_" + characterId, "drawable", this.getActivity().getPackageName());
-        Drawable charDrawable = getResources().getDrawable(resourceId);
-        imageView.setImageDrawable(charDrawable);
-    }
-
-    private void hideBackImage(ImageView frontImageView, ImageView backImageView) {
-        backImageView.setVisibility(View.GONE);
-
-        // Get rid of margins and put it in the middle
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams)frontImageView.getLayoutParams();
-        layoutParams.rightMargin = 0;
-        layoutParams.topMargin = 0;
-        layoutParams.gravity = Gravity.CENTER;
-        frontImageView.setLayoutParams(layoutParams);
-    }
-
-    private void showBackImage(ImageView frontImageView, ImageView backImageView) {
-        backImageView.setVisibility(View.VISIBLE);
-
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams)frontImageView.getLayoutParams();
-        layoutParams.rightMargin = getResources().getDimensionPixelSize(R.dimen.character_separation_amount);
-        layoutParams.topMargin = getResources().getDimensionPixelSize(R.dimen.character_separation_amount);;
-        layoutParams.gravity = Gravity.NO_GRAVITY;
-        frontImageView.setLayoutParams(layoutParams);
-    }
-
-    private void setPlayerImages(Player player, ImageView charA, ImageView charB) {
-        if (player.getCharacterA() != null && player.getCharacterB() != null) {
-            displayCharacter(player.getCharacterA().getCharacterId(), charA);
-            displayCharacter(player.getCharacterB().getCharacterId(), charB);
-            showBackImage(charA, charB);
-        } else if (player.getCharacterA() != null) {
-            displayCharacter(player.getCharacterA().getCharacterId(), charA);
-            hideBackImage(charA, charB);
-        } else {
-            charA.setImageDrawable(getResources().getDrawable(R.drawable.player_empty));
-            hideBackImage(charA, charB);
-        }
-    }
-
     private void refreshUI() {
         if (mCurrentMatch != null) {
             mEmptyText.setVisibility(View.INVISIBLE);
@@ -131,8 +86,8 @@ public class CurrentMatchFragment extends Fragment {
             mPlayerANameTextView.setText(mCurrentMatch.getPlayerA().getPerson().getName());
             mPlayerBNameTextView.setText(mCurrentMatch.getPlayerB().getPerson().getName());
 
-            setPlayerImages(mCurrentMatch.getPlayerA(), mPlayerAImageViewFront, mPlayerAImageViewBack);
-            setPlayerImages(mCurrentMatch.getPlayerB(), mPlayerBImageViewFront, mPlayerBImageViewBack);
+            mPlayerACharacter.setPlayer(mCurrentMatch.getPlayerA());
+            mPlayerBCharacter.setPlayer(mCurrentMatch.getPlayerB());
         } else {
             mEmptyText.setVisibility(View.VISIBLE);
             mPlayersContainer.setVisibility(View.INVISIBLE);
@@ -149,18 +104,6 @@ public class CurrentMatchFragment extends Fragment {
         }, Task.UI_THREAD_EXECUTOR);
     }
 
-    @SuppressWarnings("unused")
-    @OnClick({R.id.player_a_image_front, R.id.player_a_image_back})
-    void onPlayerAClick() {
-        endMatch(Player.PlayerType.PLAYER_A);
-    }
-
-    @SuppressWarnings("unused")
-    @OnClick({R.id.player_b_image_front, R.id.player_b_image_back})
-    void onPlayerBClick() {
-        endMatch(Player.PlayerType.PLAYER_B);
-    }
-
     private void selectCharacter(Player player) {
         Intent selectCharacterIntent = new Intent(getActivity(), ChooseCharactersActivity.class);
         try {
@@ -171,14 +114,27 @@ public class CurrentMatchFragment extends Fragment {
     }
 
     @SuppressWarnings("unused")
-    @OnLongClick({R.id.player_a_image_front, R.id.player_a_image_back})
+    @OnClick(R.id.player_a_character)
+    void onPlayerAClick() {
+        endMatch(Player.PlayerType.PLAYER_A);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.player_b_character)
+    void onPlayerBClick() {
+        endMatch(Player.PlayerType.PLAYER_B);
+    }
+
+
+    @SuppressWarnings("unused")
+    @OnLongClick(R.id.player_a_character)
     boolean onPlayerALongClick() {
         selectCharacter(mCurrentMatch.getPlayerA());
         return true;
     }
 
     @SuppressWarnings("unused")
-    @OnLongClick({R.id.player_b_image_front, R.id.player_b_image_back})
+    @OnLongClick(R.id.player_b_character)
     boolean onPlayerBLongClick() {
         selectCharacter(mCurrentMatch.getPlayerB());
         return true;
