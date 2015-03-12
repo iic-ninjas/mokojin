01c2cf87e3cc403bbb381f3a8f955f7ae656e0c7
Gilad Goldberg <giladgo@gmail.com>
2015-03-12 09:48:06 +0200

    Fixing some bugs

--- app/src/main/java/com/iic/mokojin/cloud/getters/GetCharacters.java
+++ app/src/main/java/com/iic/mokojin/cloud/getters/GetCharacters.java
@@ -3,11 +3,9 @@ package com.iic.mokojin.cloud.getters;
 import com.iic.mokojin.models.Character;
 import com.parse.ParseCloud;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import bolts.Continuation;
 import bolts.Task;
 
 /**
@@ -17,20 +15,7 @@ public class GetCharacters {
 
     private static final String GET_PEOPLE_CLOUD_FUNCTION_NAME = "getCharacters";
 
-    public static Task<List<com.iic.mokojin.models.Character>> getCharacters() {
-        return ParseCloud.<List<Object>>callFunctionInBackground(GET_PEOPLE_CLOUD_FUNCTION_NAME, new HashMap<String, Object>())
-                .onSuccess(new Continuation<List<Object>, List<com.iic.mokojin.models.Character>>() {
-                    @Override
-                    public List<com.iic.mokojin.models.Character> then(Task<List<Object>> task) throws Exception {
-                        List<Object> objectList = task.getResult();
-
-                        List<com.iic.mokojin.models.Character> characterList = new ArrayList<>();
-                        for (Object character : objectList) {
-                            characterList.add((Character) character);
-                        }
-
-                        return characterList;
-                    }
-                });
+    public static Task<List<Character>> getCharacters() {
+        return ParseCloud.callFunctionInBackground(GET_PEOPLE_CLOUD_FUNCTION_NAME, new HashMap<String, Object>());
     }
 }
